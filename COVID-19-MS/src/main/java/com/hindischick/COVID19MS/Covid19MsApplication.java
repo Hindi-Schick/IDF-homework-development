915a44c71a31ebf26bd4495858286791a826376f
Hindi Schick <shick02467@gmail.com>
2023-05-10 01:20:12 +0300

    with q2 - swagger

---
+++ COVID-19-MS/src/main/java/com/hindischick/COVID19MS/Covid19MsApplication.java
@@ -0,0 +1,13 @@
+package com.hindischick.COVID19MS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19MsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Covid19MsApplication.class, args);
+	}
+
+}
